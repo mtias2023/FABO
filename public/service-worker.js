67a75631797b1704509737e2e4615dd04cd99318
mtias2023 <matigutierrez2022@gmail.com>
2025-01-10 02:12:24 -0300

    Configura PWA con service worker y manifiesto

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('v1').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/img/icono-universo.png',
+          // Agrega aquí cualquier otro recurso que quieras almacenar en caché
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
-import './style.css'; 
-import './firebase'; 
+import './style.css';
+import './firebase';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import 'leaflet/dist/leaflet.css';
 
+// Registrar el Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js') // Registra el service worker
+      .then((registration) => {
+        console.log('Service Worker registrado con éxito:', registration);
+      })
+      .catch((error) => {
+        console.log('Error al registrar el Service Worker:', error);
+      });
+  });
+}
 
 createApp(App)
   .use(router)
   .mount('#app');
-
-
