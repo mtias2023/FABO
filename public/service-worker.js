000c1d6b4477e7015156de5e9737a3a7612a8095
mtias2023 <matigutierrez2022@gmail.com>
2025-01-18 00:42:54 -0300

    Actualiza service worker para mejorar el manejo de caché

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,34 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('v1').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/img/icono-universo.png',
-          // Agrega aquí cualquier otro recurso que quieras almacenar en caché
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/img/icono-universo.png',
+        '/style.css',
+        '/main.js', // Si tu JS principal es estático
+        '/login', 
+        '/registro',
+        '/bienvenidos',
+        '/nueva-publicacion',
+        '/editar-publicacion/:id',
+        '/perfil',
+        '/chat-privado/:destinatarioId/:destinatarioNombre?',
+        '/crear-partido',
+        '/partidos',
+        '/partido/:id/jugadores',
+        '/chat',
+        '/not-found', // La ruta de tu página NotFound
+        // Agregar otras rutas estáticas necesarias
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
